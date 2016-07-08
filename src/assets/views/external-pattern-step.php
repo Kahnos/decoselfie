1d25cd227a391e6412c2d6220b21759077cbae7d
José Díaz <josed1305@gmail.com>
2016-07-07 22:54:35 -0400

    Bigger images and added more padding

--- src/assets/views/external-pattern-step.php
+++ src/assets/views/external-pattern-step.php
@@ -3,26 +3,26 @@
         <h1 class="step-title" style="height: 10%;">SELECCIONE PATRÓN EXTERNO</h1>
         <div class="col-md-6 col-lg-6 eighty-height">
             <div class="step-img-parent">
-                <img class="img-responsive step-img" src="http://lorempixel.com/350/200/abstract" alt="Vista frontal">
+                <img class="img-responsive step-img" src="http://lorempixel.com/650/300/abstract" alt="Vista frontal">
             </div>
         </div>
         <div class="col-md-6 col-lg-6 eighty-height">
             <div class="step-img-parent">
-                <img class="img-responsive step-img" src="http://lorempixel.com/350/200/abstract" alt="Vista trasera">
+                <img class="img-responsive step-img" src="http://lorempixel.com/650/300/abstract" alt="Vista trasera">
             </div>
         </div>
     </div>
     <div class="row step-lower-section">
         <div class="carousel-container">
             <div class="carousel">
-                <img src="http://lorempixel.com/200/200/abstract">
-                <img src="http://lorempixel.com/200/200/nature">
-                <img src="http://lorempixel.com/200/200/animals">
-                <img src="http://lorempixel.com/200/200/city">
-                <img src="http://lorempixel.com/200/200/food">
-                <img src="http://lorempixel.com/200/200/people">
-                <img src="http://lorempixel.com/200/200/sports">
-                <img src="http://lorempixel.com/200/200/transport">
+                <img src="http://lorempixel.com/400/400/abstract">
+                <img src="http://lorempixel.com/400/400/nature">
+                <img src="http://lorempixel.com/400/400/animals">
+                <img src="http://lorempixel.com/400/400/city">
+                <img src="http://lorempixel.com/400/400/food">
+                <img src="http://lorempixel.com/400/400/people">
+                <img src="http://lorempixel.com/400/400/sports">
+                <img src="http://lorempixel.com/400/400/transport">
             </div>
         </div>
     </div>
--- src/assets/views/orientation-step.php
+++ src/assets/views/orientation-step.php
@@ -4,7 +4,7 @@
         <form role="form">
             <div class="col-md-6 col-lg-6 eighty-height">
                 <div class="step-img-parent">
-                    <img class="img-responsive step-img" src="http://lorempixel.com/350/200/abstract" alt="Horizontal">
+                    <img class="img-responsive step-img" src="http://lorempixel.com/650/300/abstract" alt="Horizontal">
                 </div>
                 <div class="radio">
                     <label for="horizontal-radio"><input type="radio" id="horizontal-radio" name="orientationRadio" value="horizontal">Horizontal</label>
@@ -12,7 +12,7 @@
             </div>
             <div class="col-md-6 col-lg-6 eighty-height">
                 <div class="step-img-parent">
-                    <img class="img-responsive step-img" src="http://lorempixel.com/200/300/abstract" alt="Vertical">
+                    <img class="img-responsive step-img" src="http://lorempixel.com/400/650/abstract" alt="Vertical">
                 </div>
                 <div class="radio">
                     <label for="vertical-radio"><input type="radio" id="vertical-radio" name="orientationRadio" value="vertical">Vertical</label>
